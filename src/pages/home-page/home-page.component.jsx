3ad14615945c85a6ec4861991526632be54f6989
Namit Doshi <namitdoshi90@gmail.com>
2020-05-27 18:52:40 +0530

    home-page set as default render page of app

---
+++ src/pages/home-page/home-page.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Header } from "../../components/header/header.component";
+
+class Home extends Component {
+  render() {
+    return (
+      <Header />
+    )};
+
+}
+
+export default Home;
