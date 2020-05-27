cc524a266b4d661e456e729d7a6634aeef9ef173
Namit Doshi <namitdoshi90@gmail.com>
2020-05-27 18:29:00 +0530

    updated navbar items

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -53,36 +53,27 @@ class Header extends Component {
             <MDBNavbarToggler onClick={this.toggleCollapse} />
             <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
               <MDBNavbarNav left>
-                <MDBNavItem active>
-                  <MDBNavLink to="#!">Home</MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink to="#!">Features</MDBNavLink>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <MDBNavLink to="#!">Pricing</MDBNavLink>
-                </MDBNavItem>
                 <MDBNavItem>
                   <MDBDropdown>
                     <MDBDropdownToggle nav caret>
-                      <span className="mr-2">Dropdown</span>
+                      <span className="mr-2">Profile</span>
                     </MDBDropdownToggle>
                     <MDBDropdownMenu>
-                      <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                      <MDBDropdownItem href="#!">Login</MDBDropdownItem>
+                      <MDBDropdownItem href="#!">Signup</MDBDropdownItem>
                     </MDBDropdownMenu>
                   </MDBDropdown>
                 </MDBNavItem>
               </MDBNavbarNav>
               <MDBNavbarNav right>
                 <MDBNavItem>
-                  <MDBFormInline waves>
-                    <div className="md-form my-0">
-                      <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-                    </div>
-                  </MDBFormInline>
+                    <MDBNavLink to="#about">About</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                    <MDBNavLink to="#testimonials">Testimonials</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                    <MDBNavLink to="upcoming-events">Upcoming Events</MDBNavLink>
                 </MDBNavItem>
               </MDBNavbarNav>
             </MDBCollapse>
