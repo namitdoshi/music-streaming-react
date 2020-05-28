654fbd20ce7bda044ce15a9dacb06f8d670697e3
Namit Doshi <namitdoshi90@gmail.com>
2020-05-28 15:46:27 +0530

    added home component on home-page

--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+
+import Home from '../../components/home/home.component';
 import AboutUs from '../../components/About-us/About-us.component'
 const HomePage = () => (
   <div className='homepage'>
-     <AboutUs/>
+    <Home/>
+    <AboutUs/>
   </div>
 );
 
