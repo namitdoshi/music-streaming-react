36b3d1c7076bb2b23a624474ca1a2354d6d626a2
Atharva Taranekar <taranekaratharva@gmail.com>
2020-05-29 12:21:25 +0530

    fixed errors .index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
-import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
-import './index.css';
-import App from './App';
 import store from './redux/store';
 
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
-  <Provider store = {store}>
-   <BrowserRouter>
-     <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
-  </Provider>
 );
-
-
---
+++ src/redux/user/user.action.js
@@ -0,0 +1,4 @@
+export const sertCurrentUser = user => ({
+    type: 'SET_CURRENT_USER' ,
+    payload: user
+});
