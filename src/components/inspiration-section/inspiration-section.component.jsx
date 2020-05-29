ab768576ed25f6692b22dccf70eda3823024b709
Namit Doshi <namitdoshi90@gmail.com>
2020-05-29 14:45:29 +0530

    completed inspiration component

--- src/components/inspiration-section/inspiration-section.component.jsx
+++ src/components/inspiration-section/inspiration-section.component.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 
 import "./inspiration-section.styles.scss";
-import guruji from '../../assets/img/guruji3.jpeg'
+import guruji from '../../assets/img/IMG-20200528-WA0011.jpg'
 
 const Inspiration = ( ) => (
   <div className = 'inspiration' id='inspiration'>
     <div className='container-fluid'>
     <h1 classname = 'inspiration-heading'>Our Inspiration</h1>
       <div className='row'>
-        <div className='col-12 col-sm-12 col-md-6 col-xl-6 guruji'>
+        <div className='col-12 col-sm-12 col-md-12 col-xl-12'>
           <img src= {guruji} alt="image of Pandit Ulhas Rajhans"/>
-        </div>
-        <div className='col-12 col-sm-12 col-md-6 col-xl-6 inspiration-text'>
-          <h2>Our beloved Guruji <b>Pandit Ulhas Rajhans</b> inspired us to create a platform to help musicians during the tough time faced by
+          <h2>Our beloved Guruji <strong>Pandit Ulhas Rajhans</strong> inspired us to create a platform to help musicians during the tough time faced by
           musicians due to sudden stopage of all concerts activities due to COVID-19. Pandit Ulhas Rajhans himself is great musician and
           an innovater who supported us to successfully create this paltform for all beloved musicians.</h2>
         </div>
