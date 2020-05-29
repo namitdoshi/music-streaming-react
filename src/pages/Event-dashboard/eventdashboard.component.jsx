522ed1b28a134df1f364d180900a9a29ab85163a
Namit Doshi <namitdoshi90@gmail.com>
2020-05-30 01:42:37 +0530

    events being retrieved from db

--- src/pages/Event-dashboard/eventdashboard.component.jsx
+++ src/pages/Event-dashboard/eventdashboard.component.jsx
@@ -2,12 +2,38 @@ import React from 'react';
 
 import EventTile from '../../components/Event-tile/event-tile.component'
 import './eventdashboard.styles.scss';
+import firebase from 'firebase';
 
 
-const EventDashboard = () => (
-  <div className='container events'>
-    <EventTile/>
-  </div>
-);
+
+// const EventDashboard = () => (
+//   <div className='container events'>
+//     <EventTile/ > 
+//   </div>
+// );
+
+class EventDashboard extends React.Component {
+  constructor () {
+    super()
+    const db = firebase.firestore();
+  //   const eventRef = db.collection('events').doc('events.id').onSnapshot(function(doc) {
+  //     console.log("Current data: ", doc.data());
+  // });
+
+  db.collection('events').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+      console.log(doc.data())
+    })
+  })
+
+  }
+  render () {
+    return (
+      <div className='container events'>
+        <EventTile/ > 
+      </div>
+    )
+  }
+}
 
 export default EventDashboard;
