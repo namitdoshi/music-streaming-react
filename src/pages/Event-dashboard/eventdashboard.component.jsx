fe40c0f3b4eab404500bceee43927bb37bcd6d0c
Namit Doshi <namitdoshi90@gmail.com>
2020-05-29 15:17:27 +0530

    event-dashboard stylesheet added

--- src/pages/Event-dashboard/eventdashboard.component.jsx
+++ src/pages/Event-dashboard/eventdashboard.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+
 import EventTile from '../../components/Event-tile/event-tile.component'
+import './eventdashboard.styles.scss';
 
 
 const EventDashboard = () => (
-   <div> <EventTile/></div>
+  <div className='container events'>
+    <EventTile/>
+  </div>
 );
 
 export default EventDashboard;
