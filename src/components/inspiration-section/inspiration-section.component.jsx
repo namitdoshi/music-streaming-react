ec039869f35396abcb3e3aa97485bac1c74f7b9c
Namit Doshi <namitdoshi90@gmail.com>
2020-05-29 12:14:23 +0530

    imported inspiration component in home page

---
+++ src/components/inspiration-section/inspiration-section.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import "./inspiration-section.styles.scss";
+import guruji from '../../assets/img/guruji3.jpeg'
+
+const Inspiration = ( ) => (
+  <p>asasa</p>
+)
+
+export default Inspiration
--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 
 import Home from '../../components/home/home.component';
 import AboutUs from '../../components/About-us/About-us.component'
+import Inspiration from '../../components/inspiration-section/inspiration-section.component'
+
 const HomePage = () => (
   <div className='homepage'>
     <Home/>
     <AboutUs/>
+    <Inspiration/>
   </div>
 );
 
