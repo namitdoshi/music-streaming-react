44fdcaab8e29e986d37ac8783a61abce2827655c
Namit Doshi <namitdoshi90@gmail.com>
2020-05-29 19:20:42 +0530

    bootstrap container used in home-component

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -5,12 +5,14 @@ import './home.styles.scss';
 import Homeimage from "../../assets/img/62236-removebg-preview.png";
 
 const Home = ( ) => (
-  <div className = 'home'>
-    <div className='home-heading'>
-      <h1>Your own Home Concert!</h1>
-    </div>
-    <div className='home-img'>
-      <img src= {Homeimage} alt="man with guitar"/>
+  <div className = 'home container-fluid'>
+    <div className='row'>
+      <div className='home-heading col-sm-12 col-md-6 col-xl-6'>
+        <h1>Your own Home Concert!</h1>
+      </div>
+      <div className='home-img col-sm-12 col-md-6 col-xl-6'>
+        <img src= {Homeimage} alt="man with guitar"/>
+      </div>
     </div>
   </div>
 );
