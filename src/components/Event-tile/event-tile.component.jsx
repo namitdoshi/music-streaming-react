5e804e8971c79a052ad426d2635f4b8c95163bf1
Namit Doshi <namitdoshi90@gmail.com>
2020-05-30 01:43:11 +0530

    props added in event-tile

--- src/components/Event-tile/event-tile.component.jsx
+++ src/components/Event-tile/event-tile.component.jsx
@@ -4,17 +4,17 @@ import {
   CardTitle, CardSubtitle,Button
 } from 'reactstrap';
 import {image} from '../../assets/img/guruji3.jpeg'
-const EventTile = () => {
+const EventTile = ({ eventTitle, artitstName, eventDate, eventTime }) => {
   return (
     <div>
       <Card>
         <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardTitle>{eventTitle}</CardTitle>
+          <CardSubtitle>{artitstName}</CardSubtitle>
         </CardBody>
         <img width='100%'  alt="Card image cap" />
         <CardBody>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <CardText></CardText>
           <Button>Click </Button>
           <Button>Artist Profile</Button>
         </CardBody>
