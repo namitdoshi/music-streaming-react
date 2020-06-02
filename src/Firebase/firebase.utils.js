b5e0b94a767b5d43285357e7ab07016276945e33
Namit Doshi <namitdoshi90@gmail.com>
2020-06-02 23:19:23 +0530

    exported firebase/storage

--- src/Firebase/firebase.utils.js
+++ src/Firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage'
 
 const config = {
   apiKey: "AIzaSyA-jrmNNgDntQjLe1IcZJGIVua11Q9fgMk",
@@ -47,7 +48,8 @@ firebase.initializeApp(config);
   
 
 export const auth = firebase.auth();
-export const firestore =firebase.firestore();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt :'select_account'});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
