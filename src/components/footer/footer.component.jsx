0c4095a55015e203eb746d3d586c04d2eacb2bee
Namit Doshi <namitdoshi90@gmail.com>
2020-06-09 14:12:50 +0530

    footer styled

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -11,15 +11,15 @@ const Footer = ( ) => (
     <div className="row">
       <div className="col-md-6 mt-md-0 mt-3 no-gutters">
         
-        <h5 className="text-uppercase">Footer Content</h5>
-        <p>Here you can use rows and columns to organize your footer content.</p>
+        <h5 className="text-uppercase text-1">Art live</h5>
+        <p className='text-2'>Always at your service</p>
       </div>
 
       <hr className="clearfix w-100 d-md-none pb-3" />
       
       <div className="col-md-3 mb-md-0 mb-3">
 
-        <h5 className="text-uppercase">Links</h5>
+        <h5 className="text-uppercase text-1">Links</h5>
 
         <ul className="list-unstyled">
           <li>
@@ -40,7 +40,7 @@ const Footer = ( ) => (
             
       <div className="col-md-3 mb-md-0 mb-3">
 
-        <h5 className="text-uppercase">Links</h5>
+        <h5 className="text-uppercase text-1">Legal</h5>
 
         <ul className="list-unstyled">
           <li>
