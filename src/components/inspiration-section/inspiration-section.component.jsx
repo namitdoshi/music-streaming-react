5cf112cba53b016881d44d564f096d8a77bf519c
Namit Doshi <namitdoshi90@gmail.com>
2020-06-10 15:13:49 +0530

    font of inspiration section updated

--- src/components/inspiration-section/inspiration-section.component.jsx
+++ src/components/inspiration-section/inspiration-section.component.jsx
@@ -9,9 +9,9 @@ const Inspiration = ( ) => (
     <div className='row'>
       <div className='col-12 col-sm-12 col-md-12 col-xl-12'>
         <img src= {guruji} alt="Pandit Ulhas Rajhans"/>
-        <h2><b>Our beloved Guruji <strong>Pandit Ulhas Rajhans</strong> inspired us to create a platform to help musicians during the tough time faced by
+        <h2>Our beloved Guruji <strong>Pandit Ulhas Rajhans</strong> inspired us to create a platform to help musicians during the tough time faced by
         musicians due to sudden stopage of all concerts activities due to COVID-19. Pandit Ulhas Rajhans himself is great musician and
-        an innovater who supported us to successfully create this paltform for all beloved musicians.</b></h2>
+        an innovater who supported us to successfully create this paltform for all beloved musicians.</h2>
       </div>
     </div>
   </div>
