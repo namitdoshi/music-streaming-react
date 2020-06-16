b29b695e172a7ff7f913d072f71ed3137c87cecf
Namit Doshi <namitdoshi90@gmail.com>
2020-06-16 14:50:35 +0530

    iframe added in musicplayer

--- src/components/player/musicplayer.js
+++ src/components/player/musicplayer.js
@@ -5,34 +5,52 @@ import { firestore } from '../../Firebase/firebase.utils'
 
 class MusicPlayer extends React.Component{
 
+  constructor () {
+    super()
+    this.state = {
+      eventURL: ''
+    }
+  }
+
   render(){
     const queryString = window.location.search;
     const eventId = queryString.slice(10)
     console.log(eventId)
 
     if (auth.currentUser != null) {
-      var eventURL = ''
+      var eventLink = ''
       firestore.collection('events').where('id', '==', eventId).get()
       .then(function(querySnapshot) {
           querySnapshot.forEach(function(doc) {
               // doc.data() is never undefined for query doc snapshots
               console.log(doc.id, " => ", doc.data());
-              eventURL = doc.data().artistImageURL
+              // eventURL = doc.data().artistImageURL
+              eventLink = 'https://player.cloud.wowza.com/hosted/kjjqhpbf/player.html'
+              this.setState({eventURL: eventLink})
           });
-          console.log(eventURL)
+          // loadScript(eventURL) 
+          console.log(this.state.eventURL)
       })
       .catch(function(error) {
           console.log("Error getting documents: ", error);
       });
+      // const URL = loadScript('//player.cloud.wowza.com/hosted/hgwpfnyc/wowza.js')
       return( 
-        <Iframe url="https://play.streamingvideoprovider.com/popplayer.php?it=aikcct6vfa8g&is_link=1&w=720&h=405&pause=1&title=Test+concert&skin=3&repeat=&brandNW=1&start_volume=100&bg_gradient1=%23ffffff&bg_gradient2=%23e9e9e9&fullscreen=1&fs_mode=2&skinAlpha=50&colorBase=%23250864&colorIcon=%23ffffff&colorHighlight=%237f54f8&direct=false&no_ctrl=&auto_hide=1&viewers_limit=0&cc_position=bottom&cc_positionOffset=70&cc_multiplier=0.03&cc_textColor=%23ffffff&cc_textOutlineColor=%23ffffff&cc_bkgColor=%23000000&cc_bkgAlpha=0.1&image=&mainBg_Color=%23ffffff&aspect_ratio=16%3A9&play_button=1&play_button_style=static&sleek_player=1&stretch="
-        width="450px"
-        height="450px"
+        
+      // <Iframe url='
+      // ' />
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+      {console.log(this.state.eventURL)}
+        <Iframe url={this.state.eventURL}
+        width="100%"
+        height="1000px"
         id="myId"
         className="myClassname"
         display="initial"
         position="relative"
         allow = 'fullscreen'/>
+      </div>
+
       )
     } else {
       return (
