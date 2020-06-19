526ade11d83a46fa134667ef299dbdfca0b3ed14
Namit Doshi <namitdoshi90@gmail.com>
2020-06-19 17:59:16 +0530

    line-height set to unset of about us and inspiration text

--- src/components/inspiration-section/inspiration-section.component.jsx
+++ src/components/inspiration-section/inspiration-section.component.jsx
@@ -9,7 +9,7 @@ const Inspiration = ( ) => (
     <div className='row'>
       <div className='col-12 col-sm-12 col-md-12 col-xl-12'>
         <img src= {guruji} alt="Pandit Ulhas Rajhans"/>
-        <h2>Our beloved Guruji <strong>Pandit Ulhas Rajhans</strong> inspired us to create a platform to help musicians during the tough time faced by
+        <h2 className='inspiration-text'>Our beloved Guruji <strong>Pandit Ulhas Rajhans</strong> inspired us to create a platform to help musicians during the tough time faced by
         musicians due to sudden stopage of all concerts activities due to COVID-19. Pandit Ulhas Rajhans himself is great musician and
         an innovater who supported us to successfully create this paltform for all beloved musicians.</h2>
       </div>
