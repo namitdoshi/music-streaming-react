71d677d8b663b82f9a389b7842029076511cecde
Namit Doshi <namitdoshi90@gmail.com>
2020-06-20 18:28:29 +0530

    font-size fixed of about and inspiration section

--- src/components/About-us/About-us.component.jsx
+++ src/components/About-us/About-us.component.jsx
@@ -11,10 +11,10 @@ const AboutUs = ( ) => (
         <img src= {laptopGuitar} alt="man with guitar and laptop"/>
       </div>
       <div className='col-12 col-sm-12 col-md-6 col-xl-6 about-us-text'>
-        <h2>Art Live is a profit sharing platform developed by the team of engineers and musicians which aims to provie a source of income
-          through digital solution for performing artists. Art Live Provides a platform for paid gigs/performances.
-          
-        </h2>
+        <span>
+        Art Live is a profit sharing platform developed by the team of engineers and musicians which aims to provie a source of income
+          through digital solution for performing artists. Art Live Provides a platform for paid gigs/performances.          
+        </span>
       </div>
     </div>  
   </div>
