d1953c84ffed3f4cd8d236eb2f35cdb5f3f43224
Namit Doshi <namitdoshi90@gmail.com>
2020-06-20 12:44:06 +0530

    added video as home component bg

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 import './home.styles.scss';
 
 import Homeimage from "../../assets/img/62236-removebg-preview.png";
+import Video from "../../assets/img/v.mp4";
 
 const Home = ( ) => (
   <div className = 'home'>
+  <video autoPlay muted loop id="myVideo">
+    <source src={Video} type="video/mp4" />
+  </video>
     <div className='row'>
       <div className='home-heading col-sm-12 col-md-6 col-xl-6'>
-        <h1>Your own Home Concert!</h1>
+        sfdmsdklfkjsdkfjk sjkfjsdj jsdfkj sdkjfj skljsdlj lkjsl
       </div>
       <div className='home-img col-sm-12 col-md-6 col-xl-6'>
-       {<img src= {Homeimage} alt="man with guitar"/>}
+       
       </div>
     </div>
   </div>
