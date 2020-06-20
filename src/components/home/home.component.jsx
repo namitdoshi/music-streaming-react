2772d6297a0b40cbae1e60acf710a48365c30ca3
Atharva Taranekar <taranekaratharva@gmail.com>
2020-06-20 15:05:14 +0530

    added carousel component

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 
 import './home.styles.scss';
+import CarouselComponent from '../Carousel/carousel.component'
 
-import Homeimage from "../../assets/img/62236-removebg-preview.png";
 
 const Home = ( ) => (
   <div className = 'home'>
     <div className='row'>
-      <div className='home-heading col-sm-12 col-md-6 col-xl-6'>
-        <h1>Your own Home Concert!</h1>
-      </div>
-      <div className='home-img col-sm-12 col-md-6 col-xl-6'>
-       <img src= {Homeimage} alt="man with guitar"/>
+      <CarouselComponent/>
       </div>
     </div>
-  </div>
+  
 );
 
 export default Home;
