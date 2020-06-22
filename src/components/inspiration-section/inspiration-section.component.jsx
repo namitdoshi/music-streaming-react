ed2ac559de6f10d74763b4246b2dafbb89f37411
Namit Doshi <namitdoshi90@gmail.com>
2020-06-22 12:32:00 +0530

    guruji text updated

--- src/components/inspiration-section/inspiration-section.component.jsx
+++ src/components/inspiration-section/inspiration-section.component.jsx
@@ -10,7 +10,7 @@ const Inspiration = ( ) => (
       <div className='col-12 col-sm-12 col-md-12 col-xl-12'>
         <img src= {guruji} alt="Pandit Ulhas Rajhans"/>
         <br/>
-        <span className='inspiration-text'>Our beloved Guruji <strong>Pandit Ulhas Rajhans</strong> inspired us to create a platform to help musicians during the tough time faced by
+        <span className='inspiration-text'>Our beloved Guruji <strong>Shri Ulhas Rajhans</strong> inspired us to create a platform to help musicians during the tough time faced by
         musicians due to sudden stopage of all concerts activities due to COVID-19. Pandit Ulhas Rajhans himself is great musician and
         an innovater who supported us to successfully create this paltform for all beloved musicians.</span>
       </div>
